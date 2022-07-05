348f9529a0d6d25c92cea3ffb9f43d09b92d9203
JpG-Dev <jessicapgasque@gmail.com>
2022-07-05 17:50:30 -0300

    Project base

---
+++ index.js
@@ -0,0 +1,33 @@
+const form     = document.querySelector("form");
+const taskList = document.querySelector("#tasks");
+const btLimpar = document.querySelector("#btLimpar")
+
+form.onsubmit = function (e) {
+	e.preventDefault();
+	const inputField = document.getElementById('task-input');
+	addTask(inputField.value);
+	form.reset();
+};
+
+function addTask(description) {
+	const taskContainer = document.createElement('div');
+	const newTask = document.createElement('input');
+	const taskLabel = document.createElement('label');
+	const taskDescriptionNode = document.createTextNode(description);
+
+	newTask.setAttribute('type', 'checkbox');
+	newTask.setAttribute('name', description);
+	newTask.setAttribute('id', description);
+
+	taskLabel.setAttribute('for', description);
+	taskLabel.appendChild(taskDescriptionNode);
+
+	taskContainer.classList.add('task-item');
+	taskContainer.appendChild(newTask);
+	taskContainer.appendChild(taskLabel);
+
+	taskList.appendChild(taskContainer);
+}
+btLimpar.addEventListener("click", () => {
+    window.location.reload();
+});
